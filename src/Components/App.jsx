b4ac41d4b9478ee6b0e1c58b19228ac7c0841f67
LaylaLindalva <138811845+LaylaLindalva@users.noreply.github.com>
2024-05-13 09:53:52 -0300

    Add files via upload

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,14 @@
-import { Button, Container, Div, Div2, Div3, Numdiv2, P2div2, P2div3, Pday, Pdiv2, Pdiv3, Pgain, Span, TituloPrincipal } from "../Style/App";
+import { Button, Container, Div, Div2, Div3, Numdiv2, P2div2, P2div3, Pday, Pdiv2, Pdiv3, Pgain, Section, Span, TituloPrincipal } from "../Style/App";
 
 
 const App = () => {
     return(
-       <Container>
+       <Container>    
         <TituloPrincipal>Join our community</TituloPrincipal>
         <Pday>30-day, hassie-íree money back guarantee</Pday>
         <Pgain>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem totam perferendis accusamus dolore illum? Consequuntur consequatur animi aliquam</Pgain>
+        
+
         <Div>
         <Div2>
          <Pdiv2>Monthly Subscription</Pdiv2>
--- src/Style/App.js
+++ src/Style/App.js
@@ -3,37 +3,67 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     width: 800px;
-    height: 500px;
-    border-radius: 20px;
+    border-radius: 10px;
     background-color: #FFF;
+    justify-content: center;
+    padding: 30px 0px 0px 0px;
+
+`
+
+export const TituloPrincipal = styled.h1`
+    color: hsl(179, 62%, 43%);
+    padding: 0px 30px 0px 30px;
+`
+
+export const Pday = styled.p`
+    color: hsl(71, 73%, 54%);
+    padding: 0px 10px 0px 30px;
+`
+
+export const Pgain = styled.p`
+    color: hsl(218, 22%, 67%);
+    padding: 0px 10px 0px 30px;
+`
+
+export const Section = styled.section`
+
 `
 
 export const Div = styled.div`
     display: flex;
+    align-items: center;
 `
 
 export const Div2 = styled.div`
     background-color: hsl(179, 62%, 43%);
+    height: 300px;
+    width: 1400px;
+    border-radius: 0px 0px 0px 10px;
 `
 
 export const Pdiv2 = styled.p`
    color: #FFF;
    font-size: 15px;
+   padding: 20px 30px 0px 30px;
 `
 
 export const Numdiv2 = styled.p`
-   font-size: 60px;
+   font-size: 30px;
    margin: 0;
    color: #FFF;
+   padding: 0px 10px 0px 30px;
 `
 
 export const Span = styled.span`
      margin: 0;
      color: #FFF;
+     font-size: 20px;
+     opacity: 80%;
 `
 
 export const P2div2 = styled.p`
      color: #FFF;
+     padding: 0px 30px 0px 30px;
 `
 
 export const Button = styled.button`
@@ -44,34 +74,31 @@ export const Button = styled.button`
     border-radius: 5px;
     color: #FFF;
     text-align: center;
-    margin: 20px;
+    margin: 20px 30px 0px 30px;
     border: 0px;
 `
 
 export const Div3 = styled.div`
     background-color: hsl(179, 62%, 43%);
     opacity: 80%;
+    height: 300px;
+    width: 1400px;
+    border-radius: 0px 0px 10px 0px;
 `
 
 export const Pdiv3 = styled.p`
    color: #FFF;
    font-size: 15px;
+   padding: 20px 30px 0px 30px;
 `
 
 export const P2div3 = styled.p`
      color: #FFF;
+     line-height: 25px;
+     opacity: 80%;
+     padding: 0px 30px;
 `
 
-export const TituloPrincipal = styled.h1`
-    color: hsl(179, 62%, 43%);
-`
 
-export const Pday = styled.p`
-    color: hsl(71, 73%, 54%);
-`
-
-export const Pgain = styled.p`
-    color: hsl(218, 22%, 67%);
-`
 
 
